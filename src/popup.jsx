9e9bb5d694280a7bc1fb5c2f64757371c52ff08b
Karapet9yanc <vkarapetiantc.exceedteam@gmail.com>
2022-02-24 11:58:10 +0300

    add time_new_word

--- src/popup.jsx
+++ src/popup.jsx
@@ -1,16 +1,52 @@
-import React from "react";
+import React, { useEffect,  useState } from "react";
 import { render } from "react-dom";
 import { allWords } from "./helpers.allWords";
-import { getRandomWord } from "./helpers.functions.jsx";
+import { shownWords } from './helpers.shownWords';
+import { getRandomWord } from './helpers.functions.jsx';
 
 const TakeWord = () => {
-  const wordAndTransl = getRandomWord(allWords);
-  return (
-    <div>
-      <h1>{wordAndTransl[0]}</h1>
-      <h2>{wordAndTransl[1]}</h2>
-    </div>
-  );
-};
+    const [wordAndTransl, setWordAndTransl] = useState(getRandomWord(allWords));
+    const [isWordClosed, setIsWordClosed] = useState(false)
+    const [word, translation] = wordAndTransl;
+
+    useEffect(() => {
+        
+    }, [wordAndTransl]);
+
+
+    const takeIt = (shownWords, allWords, word, translation) => {
+        setTimeout(() => {
+            setIsWordClosed(false);
+            setWordAndTransl(getRandomWord(allWords))
+        }, 4000);
+
+        shownWords.words.push(word);
+        shownWords.translations.push(translation);
+
+        delete allWords[word];
+
+        setIsWordClosed(true);
+    }
+
+    return (
+        <div id="wrapper">
+            {
+                isWordClosed ?
+                <p>Waiting for a next one...</p>
+                : <>
+                    <h1>{word}</h1>
+                    <h2>{translation}</h2>
+                    <button 
+                        onClick={() => takeIt(shownWords, allWords, word, translation)}
+                    >
+                        Take It
+                    </button>
+                </>
+            }
+        </div>
+    )
+}
+
 
 render(<TakeWord />, document.getElementById("react-target"));
+render(<div>Test</div>, document.querySelector('#root'))
